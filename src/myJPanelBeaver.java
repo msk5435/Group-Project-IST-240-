b0773d13319d69ec9cd6bd0a8cadbb569f4a828d
jaemib <41303925+jaemib@users.noreply.github.com>
2018-07-18 22:53:18 -0400

    Update myJPanelBeaver.java
    added theme buttons, unused
    added a home button to bring the map panel back

--- src/myJPanelBeaver.java
+++ src/myJPanelBeaver.java
@@ -2,16 +2,28 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class myJPanelBeaver extends JPanel {
+public class myJPanelBeaver extends JPanel {   
+    JButton FIRST_THEME, SECOND_THEME, THIRD_THEME, home;
     
-    JButton SubmitBeaver;
-    
-    public myJPanelBeaver () {
-        
+    public myJPanelBeaver () { 
         super();
-        setBackground(Color.PINK);
-        SubmitBeaver = new JButton("Submit Beaver Quiz");
-        add(SubmitBeaver);
-    }
-    
+        setLayout(null);
+        setBackground(Color.PINK);        
+        
+        FIRST_THEME = new JButton("Theme 1");
+        add(FIRST_THEME);
+        FIRST_THEME.setBounds(new Rectangle(50, 50, 175, 75));
+        
+        SECOND_THEME = new JButton("Theme 2");
+        add(SECOND_THEME);
+        SECOND_THEME.setBounds(new Rectangle(50, 150, 175, 75));
+        
+        THIRD_THEME = new JButton("Theme 3");
+        add(THIRD_THEME);
+        THIRD_THEME.setBounds(new Rectangle(50, 250, 175, 75));
+        
+        home = new JButton("HOME");
+        add(home);
+        home.setBounds(new Rectangle(50, 350, 175, 75));
+    }    
 }
