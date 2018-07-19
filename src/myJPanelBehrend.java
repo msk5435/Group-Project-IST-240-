d4eeb88d7c2c299692f340955663731ced501bd7
jaemib <41303925+jaemib@users.noreply.github.com>
2018-07-18 22:53:54 -0400

    Update myJPanelBehrend.java
    added theme buttons, unused
    added a home button to bring the map panel back

--- src/myJPanelBehrend.java
+++ src/myJPanelBehrend.java
@@ -3,15 +3,27 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class myJPanelBehrend extends JPanel {
+    JButton FIRST_THEME, SECOND_THEME, THIRD_THEME, home;
     
-    JButton SubmitBehrend;
-    
-    public myJPanelBehrend () {
-        
+    public myJPanelBehrend () {        
         super();
+        setLayout(null);
         setBackground(Color.darkGray);
-        SubmitBehrend = new JButton("Submit Behrend Quiz");
-        add(SubmitBehrend);
-    }
-    
+        
+        FIRST_THEME = new JButton("Theme 1");
+        add(FIRST_THEME);
+        FIRST_THEME.setBounds(new Rectangle(50, 50, 175, 75));
+        
+        SECOND_THEME = new JButton("Theme 2");
+        add(SECOND_THEME);
+        SECOND_THEME.setBounds(new Rectangle(50, 150, 175, 75));
+        
+        THIRD_THEME = new JButton("Theme 3");
+        add(THIRD_THEME);
+        THIRD_THEME.setBounds(new Rectangle(50, 250, 175, 75));
+        
+        home = new JButton("HOME");
+        add(home);
+        home.setBounds(new Rectangle(50, 350, 175, 75));
+    }    
 }
