8696e9cc6fdc4ca0e210da35a8557e026ba6c9d2
jaemib <41303925+jaemib@users.noreply.github.com>
2018-07-18 22:55:03 -0400

    Update myJPanelBrandywine.java
    added theme buttons, unused
    added a home button to bring the map panel back

--- src/myJPanelBrandywine.java
+++ src/myJPanelBrandywine.java
@@ -3,15 +3,27 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class myJPanelBrandywine extends JPanel {
+    JButton FIRST_THEME, SECOND_THEME, THIRD_THEME, home;
     
-    JButton SubmitBrandywine;
-    
-    public myJPanelBrandywine() {
-        
+    public myJPanelBrandywine () { 
         super();
-        setBackground(Color.red);
-        SubmitBrandywine = new JButton("Submit Brandywine Quiz");
-        add(SubmitBrandywine);
-    }
-    
+        setLayout(null);
+        setBackground(Color.red);        
+        
+        FIRST_THEME = new JButton("Theme 1");
+        add(FIRST_THEME);
+        FIRST_THEME.setBounds(new Rectangle(50, 50, 175, 75));
+        
+        SECOND_THEME = new JButton("Theme 2");
+        add(SECOND_THEME);
+        SECOND_THEME.setBounds(new Rectangle(50, 150, 175, 75));
+        
+        THIRD_THEME = new JButton("Theme 3");
+        add(THIRD_THEME);
+        THIRD_THEME.setBounds(new Rectangle(50, 250, 175, 75));
+        
+        home = new JButton("HOME");
+        add(home);
+        home.setBounds(new Rectangle(50, 350, 175, 75));
+    }    
 }
