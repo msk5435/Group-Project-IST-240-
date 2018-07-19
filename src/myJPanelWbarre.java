4267ffddd785689a791898d8004f9c5f72ad9e71
jaemib <41303925+jaemib@users.noreply.github.com>
2018-07-18 22:58:21 -0400

    Update myJPanelWbarre.java
    added theme buttons, unused
    added a home button to take back to the map

--- src/myJPanelWbarre.java
+++ src/myJPanelWbarre.java
@@ -3,15 +3,27 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class myJPanelWbarre extends JPanel {
+    JButton FIRST_THEME, SECOND_THEME, THIRD_THEME, home;
     
-    JButton SubmitWbarre;
-    
-    public myJPanelWbarre() {
-        
+    public myJPanelWbarre () { 
         super();
-        setBackground(Color.blue);
-        SubmitWbarre = new JButton("Submit Wilkes-Barre Quiz");
-        add(SubmitWbarre);
-    }
-    
+        setLayout(null);
+        setBackground(Color.green);        
+        
+        FIRST_THEME = new JButton("Theme 1");
+        add(FIRST_THEME);
+        FIRST_THEME.setBounds(new Rectangle(50, 50, 175, 75));
+        
+        SECOND_THEME = new JButton("Theme 2");
+        add(SECOND_THEME);
+        SECOND_THEME.setBounds(new Rectangle(50, 150, 175, 75));
+        
+        THIRD_THEME = new JButton("Theme 3");
+        add(THIRD_THEME);
+        THIRD_THEME.setBounds(new Rectangle(50, 250, 175, 75));
+        
+        home = new JButton("HOME");
+        add(home);
+        home.setBounds(new Rectangle(50, 350, 175, 75));
+    }        
 }
