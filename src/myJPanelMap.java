3a1bb0abde7c3bca34748674b2629d895ffe5e52
erdi955 <41302547+erdi955@users.noreply.github.com>
2018-07-22 17:18:12 -0400

    Update myJPanelMap.java

--- src/myJPanelMap.java
+++ src/myJPanelMap.java
@@ -1,37 +1,43 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-import javax.swing.event.*;
 
 public class myJPanelMap extends JPanel implements ActionListener {
+    ImageIcon iconmap;
+    Image map;
     JButton beaver,behrend, upark, wbarre, brandywine, menu;
         
     public myJPanelMap() {
         setLayout(null);
         setBackground(Color.orange);
         
+        
             //Buttons!    
         //beaver
         beaver = new JButton("Beaver");
         add(beaver);
-        beaver.setBounds(new Rectangle(50,50,175,50));
+        beaver.setBounds(new Rectangle(120,390,175,50));
         //behrend
         behrend = new JButton("Behrend");
         add(behrend);
-        behrend.setBounds(new Rectangle(250,50,175,50));
+        behrend.setBounds(new Rectangle(30,160,175,50));
         //upark
         upark = new JButton("University Park");
         add(upark);
-        upark.setBounds(new Rectangle(250,170,175,50));
+        upark.setBounds(new Rectangle(360,300,175,50));
         //wbarre
         wbarre = new JButton("Wilkes-Barre");
         add(wbarre);
-        wbarre.setBounds(new Rectangle(250,300,175,50));
+        wbarre.setBounds(new Rectangle(580,240,175,50));
         //brandywine
         brandywine = new JButton("Brandywine");
         add(brandywine);
-        brandywine.setBounds(new Rectangle(50,300,175,50));
-                
+        brandywine.setBounds(new Rectangle(690,440,175,50));
+       
+        //Icon iconmap = new ImageIcon(map);
+        //jlBackgroundImage = new JLabel(iconmap);
+        //jlBackgroundImage.setBounds(0,0,500,300);
+         
             //panels
         //add(pbeaver);
         /*
@@ -65,4 +71,19 @@ public class myJPanelMap extends JPanel implements ActionListener {
         brandywine.addActionListener(this);
         add(brandywine);*/        
     }
+   public void paintComponent(Graphics g) 
+	{
+    	super.paintComponent(g); 
+    	Image myImage = Toolkit.getDefaultToolkit().getImage("map-penn.jpg");
+    	g.drawImage(myImage, 0, 0, this);    	
+       
+      
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
 }
